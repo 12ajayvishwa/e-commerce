5b5364b519511e562756d0aed268924d5cb4d6dd
12ajayvishwa <ajayvish21099@gmail.com>
2023-07-14 15:46:23 +0530

    update product

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cors())
+const prepareResult = (error, data, msg) => {
+    return { success: error, data: data, message: msg };
+}
+
 app.post("/register", async (req, resp) => {
     let user = new User(req.body);
     let result = await user.save();
@@ -27,13 +31,13 @@ app.post("/login", async (req, resp) => {
         resp.send({ result: "No User Found" })
     }
 
-})
+});
 
 app.post("/add-product", async (req, resp) => {
     let product = new Product(req.body);
     let result = await product.save();
     resp.send(result);
-})
+});
 
 app.get("/get-product", async (req, resp) => {
     let productList = await Product.find()
@@ -42,11 +46,36 @@ app.get("/get-product", async (req, resp) => {
     } else {
         resp.send({ result: "No Product Found" })
     }
-})
+});
 
-app.delete("/delete-product/:id", async (req, resp) => {
-    const result = await Product.deleteOne({ _id: req.params.id })
+app.delete("/product/:id", async (req, resp) => {
+    let result = await Product.deleteOne({ _id: req.params.id })
     resp.send(result);
+});
+
+// app.get("/product/:id", async (req, resp) => {
+//     try {
+//         let result = await Product.findById({ _id: req.params.id });
+//        resp.send(result);
+//     } catch (e) {
+//         resp.send(e)
+//         console.log(e);
+//     }
+
+
+// });
+
+app.get("/product/:id", async (req, resp) => {
+    try {
+        const info = await Product.findById(req.params?.id);
+        if (info) {
+            return resp.status(200).json(prepareResult(false, info));
+        }
+        return resp.status(500).json(prepareResult(false, []));
+    } catch (error) {
+        console.error(error);
+        return resp.status(500).json(prepareResult(false, error.message));
+    }
 });
 
 app.listen(5000);
